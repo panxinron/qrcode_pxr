2051c73f793834e8e29680ace7de60dff3884b10
panxinron <1586528401@qq.com>
2018-07-06 20:06:45 +0800

    提交二维码代码

---
+++ src/com/djxs/qrcode/CreatQrcodeTest.java
@@ -0,0 +1,9 @@
+package com.djxs.qrcode;
+import java.io.IOException;
+
+import com.djxs.util.*;
+public class CreatQrcodeTest {
+    public static void main(String[]args) throws IOException{
+    	QrcodeTest.J_CreateQrcode(5, 'Q', "科技1班潘新荣", "D:/头像/pxrid.jpg", 30, "D:/二维码生成.png", "17,189,229", "160,50,168");
+    }
+}
